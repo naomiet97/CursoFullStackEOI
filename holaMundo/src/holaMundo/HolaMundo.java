7be996a8a13cc8bf43ac4bd6b75799e05025b0dd
naomiet97 <122862721+naomiet97@users.noreply.github.com>
2023-01-17 12:26:15 +0100

    Update HolaMundo.java

--- holaMundo/src/holaMundo/HolaMundo.java
+++ holaMundo/src/holaMundo/HolaMundo.java
@@ -1,10 +1,29 @@
 package holaMundo;
 
 public class HolaMundo {
+public static void ejemplosComentarios () {
+	// System.out.println("Hola Naomi");
+			/*
+			 * Este es mi primer programa
+			 * Naomi 20/05/2023
+			 */
+			System.out.println("Hola mundo!");
+	System.out.println("Otra línea");
+	System.out.println("Segunda línea");
+	System.out.println("Tercera línea");
 
+}
+
+private static void ejemplosTipos() {
+int  numero = 5;
+int numero2 = 7;
+int suma = numero + numero2;
+System.out.println(suma);
+
+}
 	public static void main(String[] args) {
-		System.out.println("Hola mundo!");
-
+		ejemplosComentarios();
+		
 	}
 
 }
